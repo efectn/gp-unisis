363e0aa59e0aacd80b48fed0c315380789f8538b
CelilA1 <celilayhan80@gmail.com>
2025-05-15 11:57:53 +0300

    Lecturer and CourseGroup repositories added

---
+++ Database/Repositories/CourseGroupRepository.cs
@@ -0,0 +1,127 @@
+﻿using gp_unisis.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace gp_unisis.Database.Repositories;
+
+public class CourseGroupsRepository
+{
+    private readonly ApplicationDbContext _context;
+
+
+    public CourseGroupsRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<CourseGroup> GetCourseGroupsByDepartmentId(int departmentId)
+    {
+        return _context.CourseGroups
+            .Include(cg => cg.Courses)
+            .Where(d => d.DepartmentId == departmentId)
+            .ToList();
+    }
+
+    public CourseGroup GetCourseGroupById(int id)
+    {
+        var courseGroup = _context.CourseGroups
+        .Include(d => d.Department)
+        .Include(cg => cg.Courses)
+        .FirstOrDefault(cg => cg.Id == id);
+
+        if (courseGroup == null)
+        {
+            throw new InvalidOperationException($"Course group with ID {id} not found.");
+        }
+
+        return courseGroup;
+    }
+
+    public void AddCourseGroups(CourseGroup courseGroup)
+    {
+        if (courseGroup == null)
+        {
+            throw new ArgumentNullException(nameof(courseGroup));
+        }
+
+        if (string.IsNullOrEmpty(courseGroup.Name))
+        {
+            throw new ArgumentException("Course name must be set.");
+        }
+
+        var existCourseGroups = _context.CourseGroups
+            .FirstOrDefault(cg => cg.Name == courseGroup.Name && cg.DepartmentId == courseGroup.DepartmentId);
+
+        if (existCourseGroups != null)
+        {
+            throw new ArgumentException("This course group is already exist in this department.");
+        }
+
+        var existDepartment = _context.Departments.FirstOrDefault(d => d.Id == courseGroup.DepartmentId);
+        if (existDepartment == null)
+        {
+            throw new InvalidOperationException($"Department with ID {courseGroup.DepartmentId} does not exist.");
+        }
+
+        _context.CourseGroups.Add(courseGroup);
+
+        try
+        {
+            _context.SaveChanges();
+        }
+        catch (DbUpdateException e)
+        {
+            if (e.InnerException != null && e.InnerException.Message.Contains("FOREIGN KEY"))
+            {
+                throw new InvalidOperationException($"Department with ID {courseGroup.DepartmentId} does not exist.");
+            }
+
+            throw;
+        }
+    }
+
+    public void UpdateCourseGroups(CourseGroup courseGroup)
+    {
+        if (courseGroup == null)
+        {
+            throw new ArgumentNullException(nameof(courseGroup));
+        }
+
+        var existCourseGroups = _context.CourseGroups.FirstOrDefault(cg => cg.Id == courseGroup.Id);
+        if (existCourseGroups == null)
+        {
+            throw new InvalidOperationException($"CourseGroups with ID {courseGroup.Id} does not exist.");
+        }
+
+        if (string.IsNullOrEmpty(courseGroup.Name))
+        {
+            throw new ArgumentException("Course name must be set.");
+        }
+
+        existCourseGroups.Name = courseGroup.Name;
+        existCourseGroups.DepartmentId = courseGroup.DepartmentId;
+
+        _context.SaveChanges();
+    }
+
+    public void DeleteCourseGroup(int id)
+    {
+        var courseGroup = _context.CourseGroups.Find(id);
+        if (courseGroup == null)
+        {
+            throw new InvalidOperationException($"Course group with ID {id} does not exist.");
+        }
+
+        _context.CourseGroups.Remove(courseGroup);
+        _context.SaveChanges();
+    }
+
+    public void AddCourseToGroup()
+    {
+        //
+    }
+
+    public void DeleteCourseFromGroup()
+    {
+        //
+    }
+}
---
+++ Database/Repositories/LecturerRepository.cs
@@ -0,0 +1,119 @@
+﻿using gp_unisis.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace gp_unisis.Database.Repositories;
+
+public class LecturerRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public LecturerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Lecturer> GetAllLecturers()
+    {
+        return _context.Lecturers.Include(d => d.Departments).ToList();
+    }
+
+    public Lecturer GetLecturerById(int id)
+    {
+        var lecturer = _context.Lecturers.Include(d => d.Departments).FirstOrDefault(l => l.Id == id);
+        if (lecturer == null)
+        {
+            throw new InvalidOperationException($"Lecturer with ID {id} not found.");
+        }
+        return lecturer;
+    }
+
+    public Lecturer GetLecturerByEmail(string email)
+    {
+        var lecturer = _context.Lecturers.Include(l => l.Departments).FirstOrDefault(l => l.Email == email);
+
+        if (lecturer == null)
+        {
+            throw new InvalidOperationException($"Lecturer with Email {email} not found.");
+        }
+
+        return lecturer;
+    }
+
+    public void AddLecturer(Lecturer lecturer)
+    {
+        if (lecturer == null)
+        {
+            throw new ArgumentNullException(nameof(lecturer));
+        }
+
+        if (string.IsNullOrEmpty(lecturer.FullName) || string.IsNullOrEmpty(lecturer.Password) || string.IsNullOrEmpty(lecturer.Email))
+        {
+            throw new ArgumentException("All required properties must be set.");
+        }
+
+        var existLecturer = _context.Lecturers.FirstOrDefault(l => l.Email == lecturer.Email);
+        if (existLecturer != null)
+        {
+            throw new InvalidOperationException($"Lecturer with Email {lecturer.Email} does already exist.");
+        }
+
+        _context.Lecturers.Add(lecturer);
+        _context.SaveChanges();
+    }
+
+    public void UpdateLecturer(Lecturer lecturer)
+    {
+        if (lecturer == null)
+        {
+            throw new ArgumentNullException(nameof(lecturer));
+        }
+
+        var existLecturer = _context.Lecturers.FirstOrDefault(l => l.Id == lecturer.Id);
+        if (existLecturer == null)
+        {
+            throw new InvalidOperationException($"Lecturer with ID {lecturer.Id} does not exist.");
+        }
+
+        existLecturer.FullName = lecturer.FullName;
+        existLecturer.Email = lecturer.Email;
+        existLecturer.Password = lecturer.Password;
+
+        _context.SaveChanges();
+    }
+
+    public void DeleteLecturer(int id)
+    {
+        var lecturer = _context.Lecturers.Find(id);
+        if (lecturer == null)
+        {
+            throw new InvalidOperationException($"Lecturer with ID {id} does not exist.");
+        }
+
+        _context.Lecturers.Remove(lecturer);
+        _context.SaveChanges();
+    }
+
+    public bool LoginLecturer(string email, string password)
+    {
+        return _context.Lecturers.Any(l => l.Email == email && l.Password == password);
+    }
+
+    public List<Announcement> GetLecturerAnnouncementsById(int id)
+    {
+        return _context.Announcements.Where(l => l.LecturerId == id).ToList();
+    }
+
+    public List<Department> GetLecturerDepartmentsById(int id)
+    {
+        var lecturer = _context.Lecturers
+            .Include(d => d.Departments)
+            .FirstOrDefault(d => d.Id == id);
+
+        if (lecturer == null)
+        {
+            throw new InvalidOperationException($"Lecturer with ID {id} not found.");
+        }
+
+        return lecturer.Departments.ToList();
+    }
+}
