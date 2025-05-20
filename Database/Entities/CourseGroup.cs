adef87b6abfb18318f642b0a518ae612477b1039
Muhammed Efe Cetin <efectn@protonmail.com>
2025-05-20 19:14:25 +0300

    add missing rows to course group

--- Database/Entities/CourseGroup.cs
+++ Database/Entities/CourseGroup.cs
@@ -4,6 +4,8 @@ public class CourseGroup
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public int RequiredCredits { get; set; }
+    public int RequiredCoursesCount { get; set; }
 
     public ICollection<Course> Courses { get; set; } = new List<Course>();
 
